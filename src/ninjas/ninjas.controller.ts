da802dee43da5284200b84b48b1d0dee66de8b75
minh711 <duyminhtruong711@gmail.com>
2024-01-26 16:08:18 +0700

    Add Guards.

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, UseGuards, ValidationPipe } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
+import { BeltGuard } from 'src/belt/belt.guard';
 
 @Controller('ninjas')
+@UseGuards(BeltGuard)
 export class NinjasController {
   constructor(private readonly ninjasService: NinjasService) {}
 
   // GET /ninjas?weapon=
   @Get()
   getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
-    
       return this.ninjasService.getNinjas(weapon);
-  
   }
 
   // GET /ninjas/:id
@@ -27,6 +27,7 @@ export class NinjasController {
 
   // POST /ninjas
   @Post()
+  @UseGuards(BeltGuard)
   createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjasService.createNinja(createNinjaDto);
   }
