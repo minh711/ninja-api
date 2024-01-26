9c2804ff80634421438f338e9a0887311ed90f56
minh711 <duyminhtruong711@gmail.com>
2024-01-26 14:31:18 +0700

    NinjasService get by weapon.

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas?weapon=
+  @Get()
+  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    const service = new NinjasService();
+    return service.getNinjas(weapon);
+  }
+
+  // GET /ninjas/:id
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  // POST /ninjas
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  // PUT /ninjas/:id
+  @Put(':id')
+  updateNinjaById(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      name: updateNinjaDto,
+    };
+  }
+
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  deleteNinjaById() {
+    return {};
+  }
+}
